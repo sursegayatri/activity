63b6d189bca80c5176d385df221209d5207961b7
Gayatri <sursegayatri@gmail.com>
2020-09-21 16:57:22 +0530

    initial commit

---
+++ src/test/java/com/db/demo/ActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
