4cb31af1429998864e55e97322287323d0eb3c4e
Gayatri <sursegayatri@gmail.com>
2020-09-21 17:31:55 +0530

    Second Commit

--- src/main/java/com/db/demo/Employee.java
+++ src/main/java/com/db/demo/Employee.java
@@ -9,7 +9,7 @@ public class Employee {
 	 String name;
 	 double salary;
 	
-	public Employee( String name, double salary) {
+	 public Employee( String name, double salary) {
 		
 		this.name = name;
 		this.salary = salary;
